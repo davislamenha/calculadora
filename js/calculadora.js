9a1d38715376ad64c2db3f5bfe3cb743f8527d78
Davis Sampaio <davislamenha@gmail.com>
2022-09-19 16:05:28 -0300

    metodo isNumber interage com display ao clicar os numeros da calculadora, metodo isOperator interage com display ao clicar nos operadores, e calcula os valores do display ao clicar no sinal de =. Formatacao do display para o calculo ser feito, bind dos metodos isNumber e isOperator em relacao a classe, e metodos bind e addEvents adicionados ao metodo init para iniciar a calculadora.

--- js/calculadora.js
+++ js/calculadora.js
@@ -10,6 +10,47 @@ export default class Calculadora {
       : this.isOperator(event);
   }
 
+  isNumber(event) {
+    if (!event.target.classList.contains("display")) {
+      const numero = event.target.innerText;
+      const displayText = this.display.innerText;
+      displayText === "0"
+        ? (this.display.innerText = numero)
+        : (this.display.innerText += numero);
+    }
+  }
+
+  isOperator(event) {
+    const operador = event.target.dataset.operador;
+    const sinal = event.target.innerText;
+    switch (operador) {
+      case "adicao":
+        this.display.innerText += sinal;
+        break;
+      case "subtracao":
+        this.display.innerText += sinal;
+        break;
+      case "multiplicacao":
+        this.display.innerText += sinal;
+        break;
+      case "divisao":
+        this.display.innerText += sinal;
+        break;
+      case "decimal":
+        this.display.innerText += sinal;
+        break;
+      case "limpar":
+        this.display.innerText = 0;
+        break;
+      case "calcular":
+        const displayFormatado = this.display.innerText
+          .replace("x", "*")
+          .replace("÷", "/");
+        this.display.innerText = eval(displayFormatado);
+        break;
+    }
+  }
+
   addEvents() {
     this.calculadora.forEach((item) => {
       item.addEventListener("click", this.onClick);
@@ -18,9 +59,13 @@ export default class Calculadora {
 
   bind() {
     this.onClick = this.onClick.bind(this);
+    this.isNumber = this.isNumber.bind(this);
+    this.isOperator = this.isOperator.bind(this);
   }
 
   init() {
+    this.bind();
+    this.addEvents();
     return this;
   }
 }
